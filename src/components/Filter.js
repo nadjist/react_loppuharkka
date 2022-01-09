9b58b53062b2399d2f91ad353819b5c19d37ff1e
nadjis <nadja.gronfors@gmail.com>
2022-01-09 14:47:58 +0200

    filter fixed, type=number, erroriin väriä, minimiarvo ja placeholder määritelty

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,8 +7,10 @@ function Filter() {
     <div>
       <label htmlFor="haku">Word count filter: </label>
       <input
-        className="shadow appearance-none border rounded w-15 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
+        className="invalid:border-red-500 invalid:border-2 shadow appearance-none border rounded w-15 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        type="number"
+        placeholder="insert number"
+        min={0}
         id="haku"
         value={wordcount}
         onChange={(e) => filterChange(e.target.value)}
